c8c2bd073dd67e3515c28decfdaf59880eaa52f7
RevanToma <tamrevan@gmail.com>
2023-02-06 12:54:31 +0100

    added routes for the categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,14 @@
-import { useContext, Fragment } from "react";
-import { CategoryContext } from "../../context/categories.context";
+import { Route, Routes } from "react-router-dom";
 
-import CategoryPreview from "../../components/category-preview/category-preview.context";
-
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.style.scss";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoryContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 export default Shop;
