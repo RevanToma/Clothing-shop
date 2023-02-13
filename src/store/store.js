c70a292ca33dc653426bbefe6eccc26b9cc970a2
RevanToma <tamrevan@gmail.com>
2023-02-13 14:32:04 +0100

    added persistedReducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,24 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
 // const middleWares = [logger];
 // const composedEnhancers = compose(applyMiddleware(...middleWares));
 // export const store = createStore(rootReducer, undefined, composedEnhancers);
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: [logger],
+  reducer: persistedReducer,
+  middleware: [process.env.NODE_ENV !== "production" && logger].filter(Boolean),
 });
+
+export const persistor = persistStore(store);
